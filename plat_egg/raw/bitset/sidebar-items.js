db99ddb8d8b97b7886ff49404e62120effd5a42c
dewert99 <33990711+dewert99@users.noreply.github.com>
2024-05-30 00:11:18 +0000

    Deploying to gh-pages from @ dewert99/plat-egg@3457c7c5168b30268bc35d1cb25182dc0fddd507 🚀

--- plat_egg/raw/bitset/sidebar-items.js
+++ plat_egg/raw/bitset/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["BitSet",""],["DefaultVec","A mapping from indexes to values where all indexes initially map to [`Default::default`]"]]};
+window.SIDEBAR_ITEMS = {"struct":[["BitSet","Set of indexes"],["DefaultVec","A mapping from indexes to values where all indexes initially map to [`Default::default`]"]]};
